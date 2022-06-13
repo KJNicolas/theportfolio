3e3e272817cf972feacbac3cc50b94dfc62097c6
Freedman J Nicolas <100964607+KJNicolas@users.noreply.github.com>
2022-06-13 08:40:36 -0700

    Main

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -1,41 +1,41 @@
 const projectsData = [
     {
-      title: "Project 1",
+      title: "Project1",
       image: "./ecommerce.svg",
       description:
         "An ecommerce application developed using react , node , redux and mongodb",
       link: "/",
     },
     {
-      title: "Project 2",
+      title: "Project2",
       image: "./pizzas.svg",
       description:
         "An ecommerce application developed using react , node , redux and mongodb",
       link: "/",
     },
     {
-      title: "Project 3",
+      title: "Project3",
       image: "./hotels.svg",
       description:
         "An ecommerce application developed using react , node , redux and mongodb",
       link: "/",
     },
     {
-      title: "Project 4",
+      title: "Project4",
       image: "./jobs.svg",
       description:
         "An ecommerce application developed using react , node , redux and mongodb",
       link: "/",
     },
     {
-      title: "Project 5",
+      title: "Project5",
       image: "./socialmedia.svg",
       description:
         "An ecommerce application developed using react , node , redux and mongodb",
       link: "/",
     },
     {
-      title: "Project 6",
+      title: "Project6",
       image: "./cars.svg",
       description:
         "An ecommerce application developed using react , node , redux and mongodb",
