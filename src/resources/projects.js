cdfd1e12e19ced802e9cd04cdfb653b10b2c4b50
Freedman J Nicolas <100964607+KJNicolas@users.noreply.github.com>
2022-06-13 08:16:39 -0700

    Main

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -1,41 +1,41 @@
 const projectsData = [
     {
-      title: "SHEYSHOP",
+      title: "Project 1",
       image: "./ecommerce.svg",
       description:
         "An ecommerce application developed using react , node , redux and mongodb",
       link: "/",
     },
     {
-      title: "SHEYPIZZA",
+      title: "Project 2",
       image: "./pizzas.svg",
       description:
         "An ecommerce application developed using react , node , redux and mongodb",
       link: "/",
     },
     {
-      title: "SHEYROOMS",
+      title: "Project 3",
       image: "./hotels.svg",
       description:
         "An ecommerce application developed using react , node , redux and mongodb",
       link: "/",
     },
     {
-      title: "SHEYJOBS",
+      title: "Project 4",
       image: "./jobs.svg",
       description:
         "An ecommerce application developed using react , node , redux and mongodb",
       link: "/",
     },
     {
-      title: "SHEYGRAM",
+      title: "Project 5",
       image: "./socialmedia.svg",
       description:
         "An ecommerce application developed using react , node , redux and mongodb",
       link: "/",
     },
     {
-      title: "SHEYCARS",
+      title: "Project 6",
       image: "./cars.svg",
       description:
         "An ecommerce application developed using react , node , redux and mongodb",
